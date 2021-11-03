528a5207d7bb212eb4dbef8c229607f7b7af3c18
Crowdbotics <engineering@crowdbotics.com>
2021-11-03 17:17:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/privacytest_dev_23071/MainActivity.java
@@ -0,0 +1,15 @@
+package com.privacytest_dev_23071;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "privacytest_dev_23071";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "privacytest_dev_23071",
+  url: "https://privacytest_dev_23071.botics.co",
+  api: "https://privacytest_dev_23071.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
